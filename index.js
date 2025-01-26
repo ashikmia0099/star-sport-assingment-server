e47368760983657f42d08f013bc6f71a29a8a847
ashikmia0099 <ashikmia5686@gmail.com>
2025-01-26 11:13:33 +0600

    put my data

--- index.js
+++ index.js
@@ -84,6 +84,38 @@ async function run() {
         res.send(result);
       })
 
+
+        // put my posted equepment
+
+  
+      app.put('/cards/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updatedComponentDoc = req.body;
+  
+        // akhane update turor doc a data set kora holo
+  
+        const componentDoc = {
+          $set: {
+            
+            name: updatedComponentDoc.name,
+            email: updatedComponentDoc.email,
+            itemname: updatedComponentDoc.itemname,
+            categoryName: updatedComponentDoc.categoryName,
+            price: updatedComponentDoc.price,
+            Rating: updatedComponentDoc.Rating,
+            customization: updatedComponentDoc.customization,
+            deliverytime: updatedComponentDoc.deliverytime,
+            Image: updatedComponentDoc.Image,
+            ProductQuentity: updatedComponentDoc.ProductQuentity,
+            description: updatedComponentDoc.description,
+          },
+        };
+  
+        const result = await ShopCardCollection.updateOne(filter, componentDoc, options);
+        res.send(result);
+      });
   
   
   
